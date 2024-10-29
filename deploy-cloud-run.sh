f31ea6b2c2869fb8c29d46fc75ffafcbc2c46620
jwizenf2 <156966838+jwizenf2@users.noreply.github.com>
2024-10-29 12:41:45 -0400

    Added deployment file

---
+++ deploy-cloud-run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Run following commands to deploy
+# chmod +x deploy-cloud-run.sh (first time needed to give executable permissions)
+# ./deploy-cloud-run.sh
+
+# Load environment variables from .env file
+set -o allexport
+source .env
+set +o allexport
+
+# Full image path
+FULL_IMAGE_NAME="gcr.io/$PROJECT_ID/$IMAGE_NAME"
+
+# Build and push the Docker image
+docker buildx build --platform linux/amd64 -t $FULL_IMAGE_NAME --push .
+
+# Deploy to Cloud Run with environment variables, secret, and volume mounts
+gcloud run deploy $SERVICE_NAME \
+  --image $FULL_IMAGE_NAME \
+  --platform managed \
+  --region $REGION \
+  --allow-unauthenticated \
+  --set-env-vars FLASK_SECRET_KEY="$FLASK_SECRET_KEY" \
+  --set-env-vars APP_ENV="$APP_ENV" \
+  --set-env-vars GOOGLE_CLOUD_PROJECT_ID="$GOOGLE_CLOUD_PROJECT_ID" \
+  --set-env-vars FIRESTORE_COLLECTION_NAME="$FIRESTORE_COLLECTION_NAME" \
+  --set-env-vars FIREBASE_STORAGE_BUCKET="$FIREBASE_STORAGE_BUCKET" \
+  --set-env-vars GOOGLE_APPLICATION_CREDENTIALS="$SECRET_PATH" \
+  --update-secrets $SECRET_PATH=$SECRET_NAME:latest
+
+
+
+
+
